99543fed0fcc665f07f5e6f62dec8a4d24f051e3
LWS0901 <aceterry01@naver.com>
2024-05-01 11:50:34 +0900

    week04_이원석

---
+++ week04/이원석/과제 4주차 추가문제.js
@@ -0,0 +1,25 @@
+//5번
+// var messageContainer = document.getElementById("messageContainer");
+// var addButton = document.getElementById("addMessage");
+// var userInput = document.getElementById("userInput");
+
+// addButton.addEventListener("click",()=>{
+//   var messageText = userInput.value; 
+//   console.log(messageText)
+//  if (messageText !== "") { //공백이 아니라면
+//     var p = document.createElement("p");
+//     p.textContent = messageText; 
+//     messageContainer.appendChild(p); 
+//     userInput.value = ""; 
+//  }
+// });
+
+//6번
+var htmlContainer = document.getElementById("htmlContainer");
+var updateButton = document.getElementById("updateHtml");
+var htmlInput = document.getElementById("htmlInput");
+
+updateButton.addEventListener("click",()=> {
+  var userInputHtml = htmlInput.value; 
+  htmlContainer.innerHTML = userInputHtml; 
+});
